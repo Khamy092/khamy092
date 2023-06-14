ffdd55d498ff5baeff5079f5ed1e3ab0001cd422
khamy092 <khamy092@mymail.unisa.edu.au>
2023-06-14 14:54:21 +0930

    initialisers are added.

--- Workshop.py
+++ Workshop.py
@@ -9,14 +9,33 @@ StudentID: 110341761
 EmailID: khamy092
 This is my own work as defined by the University's Academic Misconduct Policy.
 '''
-
+from Materials import *
+from Weapon import *
 
 class Workshop:
-    def __init__(self) -> None:
-        pass
+
+    '''
+    This is the constructor for the Workshop class. It initializes the Enchanter and Forge objects.
+    It also initializes the weapons, materials and enchantments lists as well as the materials and enchantments dictionaries.
+    The workshop class is the main class in the system. It uses other classess to create, modify and delete weapons. It also adds and deletes materials.
+    Workshop is also responsible for taking care of the inventory of the weapons, materials and enchantments.
+    '''
+    def __init__(self, enchanter, forge):
+        
+        self.enchanter = enchanter
+        self.forge = forge
+        self._weapons = []
+        self._materials = set()
+        self._enchantments = {}
+
+    # Getters and setters for the weapons, materials and enchantments lists and dictionaries.
 
     def displayWeapons(self):
-        pass
+        for weapon in self._weapons:
+            if weapon is Weapon.isEnchanted:
+                print(weapon.getName() + " (Enchanted)")
+            
+            
 
     def displayEnchantments(self):
         pass
@@ -41,4 +60,3 @@ class Workshop:
 
     def removeMaterial(self):
         pass
-    
