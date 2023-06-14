03f74f5c997edd2c5974fbbd8032f7e0642d424e
khamy092 <khamy092@mymail.unisa.edu.au>
2023-06-14 19:42:25 +0930

    method s' implementations added.

--- Workshop.py
+++ Workshop.py
@@ -11,6 +11,10 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 '''
 from Materials import *
 from Weapon import *
+from Enchantment import *
+from Enchanter import *
+from Forge import *
+
 
 class Workshop:
 
@@ -25,38 +29,55 @@ class Workshop:
         self.enchanter = enchanter
         self.forge = forge
         self._weapons = []
-        self._materials = set()
+        self._materials = {}
         self._enchantments = {}
 
-    # Getters and setters for the weapons, materials and enchantments lists and dictionaries.
-
+    # display all the weapons in the workshop
     def displayWeapons(self):
         for weapon in self._weapons:
             if weapon is Weapon.isEnchanted:
                 print(weapon.getName() + " (Enchanted)")
-            
-            
 
+            
+            
+    # display all the enchantments in the workshop
     def displayEnchantments(self):
-        pass
+        for enchantment in self._enchantments:
+            print(f'An {enchantment.getEnchantmentName()} enchantment is stored in the workshop.')
 
+    # display all the materials in the workshop
     def displayMaterials(self):
-        pass
+        for material in self._materials:
+            print(f'A {material.__class__.__name__,}: {self._materials[material]} remaining.')
 
-    def addWeapon(self):
-        pass
+    # add a weapon to the workshop
+    def addWeapon(self, Weapon):
+        self._weapons.append(Weapon)
+        return self._weapons
 
-    def removeWeapon(self):
-        pass
+    # remove a weapon from the workshop
+    def removeWeapon(self, weapon):
+        self._weapons.remove(weapon)
+        return self._weapons
 
-    def addEnchantment(self):
-        pass
+    # add an enchantment to the workshop
+    def addEnchantment(self, enchantment):
+        self._enchantments.append(enchantment)
+        return self._enchantments
+    
+    # remove an enchantment from the workshop
+    def removeEnchantment(self, enchantment):
+        del self._enchantments[enchantment]
+        return self._enchantments
 
-    def removeEnchantment(self):
-        pass
+    # add a material to the workshop
+    def addMaterial(self, material, quantity):
+        self._materials[material] = quantity
+        return self._materials
 
-    def addMaterial(self):
-        pass
+    # remove a material from the workshop
+    def removeMaterial(self, material):
 
-    def removeMaterial(self):
-        pass
+        # remove the material from the dictionary
+        del self._materials[material]
+        return self._materials
