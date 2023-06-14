00136e20354efb4130a9d5e28b5efd04a408e8ef
khamy092 <khamy092@mymail.unisa.edu.au>
2023-06-14 12:05:39 +0930

    empty methods added.

--- Workshop.py
+++ Workshop.py
@@ -9,3 +9,36 @@ StudentID: 110341761
 EmailID: khamy092
 This is my own work as defined by the University's Academic Misconduct Policy.
 '''
+
+
+class Workshop:
+    def __init__(self) -> None:
+        pass
+
+    def displayWeapons(self):
+        pass
+
+    def displayEnchantments(self):
+        pass
+
+    def displayMaterials(self):
+        pass
+
+    def addWeapon(self):
+        pass
+
+    def removeWeapon(self):
+        pass
+
+    def addEnchantment(self):
+        pass
+
+    def removeEnchantment(self):
+        pass
+
+    def addMaterial(self):
+        pass
+
+    def removeMaterial(self):
+        pass
+    
