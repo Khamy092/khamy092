f873e5f67d352a65ac954b497a34c54382c4cef1
khamy092 <khamy092@mymail.unisa.edu.au>
2023-06-16 00:46:11 +0930

    attributes, methods, and setters and getters are added.

--- Enchantment.py
+++ Enchantment.py
@@ -13,35 +13,63 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 
 
 class Enchantment:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name, magicDamage, effect, primaryMaterial, catalystMaterial):
+        self._name = name
+        self._magicDamage = magicDamage
+        self._effect = effect
+        self._primaryMaterial = primaryMaterial
+        self._catalystMaterial = catalystMaterial
 
-    def setEnchantmentName(self):
-        pass
 
-    def getEnchantmentName(self):
-        pass
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
 
-    def setEnchantmentEffect(self):
-        pass
+    @property
+    def magicDamage(self):
+        return self._magicDamage
+    
+    @magicDamage.setter
+    def magicDamage(self, magicDamage):
+        self._magicDamage = magicDamage
 
-    def getEnchantmentEffect(self):
-        pass
+    @property
+    def effect(self):
+        return self._effect
+    
+    @effect.setter
+    def effect(self, effect):
+        self._effect = effect
 
-    def getMagicDamage(self):
-        pass
+    @property
+    def primaryMaterial(self):
+        return self._primaryMaterial
+    
+    @primaryMaterial.setter
+    def primaryMaterial(self, primaryMaterial):
+        self._primaryMaterial = primaryMaterial
 
-    def setMagicDamage(self):
-        pass
-
-    def getPrimaryMaterial(self):
-        pass
-
-    def getCatalystMaterial(self):
-        pass
+    @property
+    def catalystMaterial(self):
+        return self._catalystMaterial
+    
+    @catalystMaterial.setter
+    def catalystMaterial(self, catalystMaterial):
+        self._catalystMaterial = catalystMaterial
 
     def calculateDamage(self):
-        pass
+        '''
+        This method calculates the damage of the enchantment.
+        '''
+        damage = self.primaryMaterial.strength * self.primaryMaterial.MagicPower + self.catalystMaterial.strength * self.catalystMaterial.MagicPower
+        return damage
 
     def useEffect(self):
-        pass
+        '''
+        This method applies the effect of the enchantment.
+        '''
+        return f'The {self.name} enchantment {self.effect}'
