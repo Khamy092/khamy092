de9161417c798665b4355b8db913bed9092b4548
khamy092 <khamy092@mymail.unisa.edu.au>
2023-06-15 15:59:46 +0930

    Forge module initial implementation

--- Forge.py
+++ Forge.py
@@ -12,14 +12,41 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 '''
 from Weapon import *
 from Crafter import Crafter
+from Workshop import Workshop
 
 class Forge(Crafter):
-    def __init__(self):
+    def __init__(self, name, primaryMaterial, catalystMaterial):
         super().__init__()
+        Weapon.__init__(self, name, primaryMaterial, catalystMaterial)
 
-    def craft(self):
-        pass
-    
-    def disassemble(self):
-        pass
+    def craft(self, name, primaryMaterial, catalystMaterial):
+        '''
+        This method is used to create a weapon. It takes in the name, primary material and catalyst material as parameters.
+        It then creates a weapon object and adds it to the weapons list in the Workshop class.
+        It also removes the materials from the materials dictionary to reflect the change in the inventory.
+        '''
+        
+        # create a weapon object
+        craftedWeapon = Weapon(name, primaryMaterial, catalystMaterial, Workshop.materials)
+        # add the weapon to the weapons list
+        Workshop.addWeapon(craftedWeapon)
+        # remove the materials from the materials dictionary
+        materials = Workshop.materials
+        materials[primaryMaterial] -= 1
+        materials[catalystMaterial] -= 1
+
+    def disassemble(self, weapon):
+        '''
+        This method is used to delete a weapon. It takes in the weapon to be deleted as a parameter.
+        It then removes the weapon from the weapons list in the Workshop class.
+        It also adds the materials from the materials dictionary to reflect the change in the inventory.
+        '''
+        
+        # remove the weapon from the weapons list
+        Workshop.removeWeapon(weapon)
+        
+        # add the materials from the materials dictionary
+        materials = Workshop.materials
+        materials[weapon.primaryMaterial] += 1
+        materials[weapon.catalystMaterial] += 1
     
